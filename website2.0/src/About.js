bced1426f4e60f140d8f20ebb561f093afb7fc73
Gurjot Singh <gurjot@myerrunds.com>
2019-01-01 03:08:20 -0500

    created footer

---
+++ website2.0/src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import './About.css';
+
+
+class Navbar extends Component {
+    render() {
+      return (
+        <div>Hello</div>
+      )
+    }
+  }
+
+export default Navbar;
--- website2.0/src/App.js
+++ website2.0/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './Navbar.tsx';
+import Navbar from './Navbar.js';
+import About from './About.js';
+import Footer from './Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div>
       <Navbar />
+      <About />
+      <Footer />
       
       </div>
     )
