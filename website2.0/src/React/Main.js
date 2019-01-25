ead7e7fd5654201cdcacd20e4c69cd78a12efe9a
Gurjot Singh <gurjot@myerrunds.com>
2019-01-24 23:59:47 -0500

    added image / writting on main page

--- website2.0/src/React/Main.js
+++ website2.0/src/React/Main.js
@@ -13,10 +13,15 @@ class Main extends Component {
           <br/>
           <br/>
           <br/>
-          <br/>
             <span className='mp-txt'>Hey!</span><br/>
             <span className='mp-txt'>I'm Gurjot,</span><br/>
             <span className='mp-txt'>And I'm a Web Developer</span>
+          </div>
+          <br/>
+          <div className='mp-info-div'>
+          <span className='mp-info-text'>I Specialize in</span>
+          <br/>
+          <span className='mp-info-text'>HTML/CSS, JavaScript, React</span>
           </div>
         </div>
       )
